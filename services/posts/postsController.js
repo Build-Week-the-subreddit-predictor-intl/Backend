4b5c97fd52be353491f05764e64a705fc125176c
richanynguon <alina.nguon@gmail.com>
2019-11-18 13:50:18 -0500

    complete controllers

--- services/posts/postsController.js
+++ services/posts/postsController.js
@@ -37,7 +37,7 @@ const fetchPostById = (req, res) => {
 const makePost = (req, res) => {
 	const post = req.body;
 	insert(post)
-		.then(response => {
+		.then(() => {
 			res.status(201).json({
 				message: `Succefully posted ${post.title} ${post.text}`
 			});
@@ -46,6 +46,39 @@ const makePost = (req, res) => {
 			res.status(400).json({
 				message: `Unable to process request to create post because
         ${err.message}`
+			});
+		});
+};
+
+const updatePost = (req, res) => {
+	const { id } = req.params;
+	const post = req.body;
+
+	update(id, post)
+		.then(() => {
+			res.status(200).json({
+				message: `Succefully updated post ${id} with ${post.title} ${post.text}`
+			});
+		})
+		.catch(err => {
+			res.status(400).json({
+				message: `Unable to process request to update post ${id} because
+        ${err.message}`
+			});
+		});
+};
+
+const removePost = (req, res) => {
+	const { id } = req.params;
+	remove(id)
+		.then(() => {
+			res.status(200).json({
+				message: `Post ${id} was succesfully removed`
+			});
+		})
+		.catch(err => {
+			res.status(400).json({
+				error: `Unable to process removal because of ${err.message}`
 			});
 		});
 };
