181ebbaa465762f29ed0aa798d026c3a83aaaf74
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-19 18:50:42 +0000

    Added fetch of user based on state coming from reddit redirect

--- services/reddit/redditRouter.js
+++ services/reddit/redditRouter.js
@@ -1,19 +1,20 @@
 const config = require('../../config');
 const axios = require('axios');
-const bcrypt = require('bcryptjs');
 const redditRouter = require('express').Router();
 // Helpers
-const { updateUser } = require('../auth/authModel');
+const { findUser, updateUser } = require('../auth/authModel');
 const {
   requireLogin,
   handleErrors,
-  authorizeRedditAccess,
-  objectToQueryString
+  objectToQueryString,
+  toBase64,
+  toUTF8
 } = require('../global/globalHelpers');
+const { authorizeRedditAccess } = require('./redditController');
 // Endpoints
 redditRouter.get('/', requireLogin, (req, res, next) => {
   const { mobile } = req.query;
-  res.status(200).json({ url: authorizeRedditAccess(mobile ? true : false) });
+  res.status(200).json({ url: authorizeRedditAccess(req.loggedInUser.subject, mobile ? true : false) });
 });
 
 redditRouter.get('/auth', async (req, res, next) => {
@@ -23,28 +24,29 @@ redditRouter.get('/auth', async (req, res, next) => {
       next({ message: error, status: 403 });
       return;
     }
-    const isValidState = bcrypt.compareSync(config.redditState, decodeURIComponent(state));
-    if (!isValidState) {
+    const validState = JSON.parse(toUTF8(state)); // TODO: IMPORTANT missing check if state has been tampered with!!!
+    if (!validState || !validState.id) {
       next({ message: "Invalid state/code. Data has been compromised.", status: 401 });
       return;
     }
+    let user = await findUser({ id: validState.id });
     let payload = {
-      grant_type: 'authorization_code', // or 'refresh_token' if user has auth token
+      grant_type: (!user.access_token ? 'authorization_code' : 'refresh_token'), 
       redirect_uri: config.redditRedirectURL,
     };
     if (code) {
       payload.code = code;
     }
     payload = objectToQueryString(payload, false);
-    let buffer = Buffer.from(config.redditClientId + ':' + config.redditClientSecret).toString('base64');
-    let options = { headers: { Authorization: 'Basic ' + buffer } };
+    let auth = toBase64(config.redditClientId + ':' + config.redditClientSecret);
+    let options = { headers: { Authorization: 'Basic ' + auth } };
     const redditAccess = await axios.post('https://www.reddit.com/api/v1/access_token', payload, options)
     if (redditAccess.data.error) {
       next({ message: redditAccess.data.error });
       return;
     }
     // save access_token, refresh_token to user table | token_type, expires_in, scope
-    const updatedUser = await updateUser(redditAccess.data, req.loggedInUser.subject);
+    const updatedUser = await updateUser(redditAccess.data, user.id);
     if (updatedUser.data && updatedUser.data.id) {
       res.status(200).json({ authorized: true });
     } else {
